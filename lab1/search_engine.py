c8cf69b81a1d4fb1382c16b9d941f75c8fa4b75c
devops4us <g.luef@gmx.at>
2025-04-23 09:19:00 +0200

    intermediate

--- lab1/search_engine.py
+++ lab1/search_engine.py
@@ -5,15 +5,18 @@ class Document:
         self.title=title
         self.text=text
 
+    def __repr__(self) ->str:
+        return f"Document(id={self.id} title={self.title})"    
+
 class DocumentIndex:
 
     def __init__(self):
         pass
 
-    def add_to_index(doc:Document) -> None:
+    def add_to_index(self,doc:Document) -> None:
         pass
 
-    def find_token(token:str) -> list[Document]:
+    def find_token(self,token:str) -> list[Document]:
         pass
 
 class QueryProcessor:
@@ -21,11 +24,54 @@ class QueryProcessor:
     def __init__(self, index:DocumentIndex):
         self.index=index
 
-    def query(query:str) -> list[Document]:    
+    def query(self,query:str) -> list[Document]:    
         pass
 
+def extract_normalized_tokens(text:str) -> list[str]:
+    
+
+def initialize_data(documents:list[Document], questions:list[dict]) -> None:
+    from csv import DictReader
+    doc_lines= [
+    "id;title;text",
+    "1;Oldest Known Song; The oldest known song is the Hurrian Hymn No. 6, which dates back to around 1400 BCE.",
+    "2;Bohemian Rhapsody; The iconic song Bohemian Rhapsody, released in 1975, is known for its unique structure and lack of a traditional chorus.",
+    "3;Mozart's Early Start; Wolfgang Amadeus Mozart composed his first piece of music at the age of five.",
+    "4;Eye of the Tiger; The iconic rock song Eye of the Tiger is known for its motivational lyrics and driving rhythm.",
+    "5;Music and the Brain; Listening to music can stimulate the brain and improve memory, mood, and cognitive function.",
+    "6;Universal Language; Music is often referred to as a universal language because it can convey emotions and stories without words.",
+    "7;Largest Orchestra; The largest orchestra ever assembled consisted of 8,097 musicians and performed in Frankfurt, Germany, in 2019.",
+    "8;Music Therapy; Music therapy is used to help patients with various conditions, including depression, anxiety, and chronic pain.",
+    "9;Birds and Music; Some birds, like the lyrebird, can mimic musical instruments and human-made sounds.",
+    "10;Quintet; A team of five people can form a band and create music as a quintet."
+    ]
+    documents.extend(list(DictReader(doc_lines, delimiter=';', skipinitialspace=True)))
+
+    question_lines =[
+    "question;doc;method"
+    "tell me the oldest song title;1;keyword-search",
+    "what was the first vocal ever sung;1;synonyms",
+    "can animals make music;9;meronyms",
+    "what was the first song;1;word-vector-search",
+    "can music bring me back to an active life;8;passage-retrieval",
+    "can a five years old make music;3;passage-retrieval",
+    "is there music about animals;4;passage-retrieval",    
+    ]
+    questions.extend(list(DictReader(question_lines, delimiter=';', skipinitialspace=True)))
+   
 if __name__ == "__main__":
 
+    documents=[]
+    queries=[]
+    initialize_data(documents,queries)
+    print(f"read {len(documents)} documents and {len(queries)} queries")
+
     index= DocumentIndex()
+    for doc in documents:
+        index.add_to_index(Document(**doc))
     query_processor= QueryProcessor(index)
-    print ("search not implemented yet")
+
+    for q in queries:
+        result= query_processor.query(q["question"])
+        #result=[Document(**doc) for doc in documents[0:2]]
+        print(f"QUERY={q["question"]}, RESULT={result}")
