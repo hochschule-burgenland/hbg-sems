74817a28f5c0d41fae37fbb83698a2393150a7df
devops4us <g.luef@gmx.at>
2025-04-24 15:15:11 +0200

    changes

---
+++ lab1/passage_retrieval.py
@@ -0,0 +1,49 @@
+from sentence_transformers import SentenceTransformer, util
+
+def initialize_data(documents:list[dict], questions:list[dict]) -> None:
+    from csv import DictReader
+    doc_lines= [
+    "id;title;text",
+    "1;Oldest Known Song; The oldest known song is the Hurrian Hymn No. 6, which dates back to around 1400 BCE.",
+    "2;Bohemian Rhapsody; The iconic song Bohemian Rhapsody, released in 1975, is known for its unique structure and lack of a traditional chorus.",
+    "3;Mozart's Early Start; Wolfgang Amadeus Mozart composed his first piece of music at the age of five.",
+    "4;Eye of the Tiger; The iconic rock song Eye of the Tiger is known for its motivational lyrics and driving rhythm.",
+    "5;Music and the Brain; Listening to music can stimulate the brain and improve memory, mood, and cognitive function.",
+    "6;Universal Language; Music is often referred to as a universal language because it can convey emotions and stories without words.",
+    "7;Largest Orchestra; The largest orchestra ever assembled consisted of 8,097 musicians and performed in Frankfurt, Germany, in 2019.",
+    "8;Music Therapy; Music therapy is used to help patients with various conditions, including depression, anxiety, and chronic pain.",
+    "9;Birds and Music; Some birds, like the lyrebird, can mimic musical instruments and human-made sounds.",
+    "10;Quintet; A team of five people can form a band and create music as a quintet."
+    ]
+    documents.extend(list(DictReader(doc_lines, delimiter=';', skipinitialspace=True)))
+
+    question_lines =[
+    "question;doc;method",
+    "what are very old songs;1;keyword-search",
+    "what was the first vocal ever sung;1;synonyms",
+    "can animals make music;9;meronyms",
+    "what was the first song;1;word-vector-search",
+    "can music bring me back to an active life;8;passage-retrieval",
+    "can a five years old make music;3;passage-retrieval",
+    "is there music about animals;4;passage-retrieval",    
+    ]
+    questions.extend(list(DictReader(question_lines, delimiter=';', skipinitialspace=True)))
+
+if __name__ == "__main__":
+
+    documents=[]
+    queries=[]
+    initialize_data(documents,queries)
+
+    model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+    doc_vectors= model.encode([doc["text"] for doc in documents])
+    query_vectors= model.encode([q["question"] for q in queries])
+
+    results= util.semantic_search(query_vectors, doc_vectors)
+    for i in range(len(query_vectors)):
+        print(queries[i])
+        docindex=results[i][0]["corpus_id"]
+        print(documents[docindex])
+        print("-----------")
+
+    
